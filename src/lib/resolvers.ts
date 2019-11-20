25fcf0e4adb7db4f6af4b9fffb9897cd33cce528
JaimeCardenas176 <jaime.cardenas176@gmail.com>
2019-11-20 14:00:51 +0100

    continuamos para bingo

--- src/lib/resolvers.ts
+++ src/lib/resolvers.ts
@@ -1,20 +1,20 @@
 const courses = [
     {
-    _id: '1',
+    _id: 'zsdf1',
     title:'graphql',
     teacher:'Jaime',
     description: 'descripcion del curso de graphql',
     topic:'desarrollo'
     },
     {
-    _id: '2',
+    _id: 'ser2',
     title:'nodeJS',
     teacher:'David',
     description: 'descripcion del curso de nodeJS',
     topic:'desarrollo'
     },
     {
-    _id: '3',
+    _id: 'ser3',
     title:'python',
     teacher:'Carlos',
     description: 'descripcion del curso de python',
