6a04098a6b1e9a94f2b8c0cc20eed38937636c9b
JaimeCardenas176 <jaime.cardenas176@gmail.com>
2019-11-20 16:48:25 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -8,13 +8,13 @@ const resolvers = require('./lib/resolvers')
 const app = express()
 const port = process.env.port || 3000
 const endpoint = '/api'
+
 // definiendo el esquema
-const schema = buildSchema(
-  readFileSync(
+const typeDefs = readFileSync(
     join(__dirname, 'lib', 'schema.graphql'),
     'utf-8'
-  )
 )
+const schema = makeExecutableSchema({typeDefs, resolvers});
 
 app.use(endpoint, gqlMiddleware({
   schema: schema,
